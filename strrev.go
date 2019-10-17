3907bc8a0d010fe031837ad988ddcac13b3f1b56
kanakz <kablanov.kk@gmail.com>
2019-10-17 16:48:19 +0600

    strev

--- strrev.go
+++ strrev.go
@@ -1,12 +1,14 @@
 package piscine
 
-import ()
+import (
+)
 
 func StrRev(s string) string {
 
-	var.reverse string
-for i := len(s)-1; >= 0; i-- {
-	reverse += string(s[i])
-}
-return	reverse
+
+    var reverse string
+    for i := len(s)-1; i >= 0; i-- {
+        reverse += string(s[i])
+    }
+    return reverse 
 }
