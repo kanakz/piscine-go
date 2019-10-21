dcb155a2375ef35d55727594e805c3d248dba344
kanakz <52851831+kanakz@users.noreply.github.com>
2019-10-21 21:12:00 +0600

    Update recursivefactorial.go

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,1 +1,13 @@
+package piscine
 
+func RecursiveFactorial(nb int) int {
+	if nb < -20 || nb > 20 {
+		return 0
+	} else if nb == 1 || nb == 0 {
+		return 1
+	} else if nb < 0 {
+		return 0
+	} else {
+		return nb * RecursiveFactorial(nb-1)
+	}
+}
