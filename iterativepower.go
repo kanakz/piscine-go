d2c5a6b7ececc7d510080b6aaf76c32fa41805c0
kanakz <52851831+kanakz@users.noreply.github.com>
2019-10-21 21:24:12 +0600

    Create iterativepower.go

---
+++ iterativepower.go
@@ -0,0 +1,15 @@
+ackage piscine
+
+func IterativePower(nb, power int) int {
+	if power == 0 {
+		return 1
+	} else if power < 0 {
+		return 0
+	} else {
+		res := 1
+		for i := 1; i <= power; i++ {
+			res *= nb
+		}
+		return res
+	}
+}
