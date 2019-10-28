7159903be17b12996ac1832edf42ff9b30900509
kanakz <52851831+kanakz@users.noreply.github.com>
2019-10-28 21:43:00 +0600

    Create basicatoi2.go

---
+++ basicatoi2.go
@@ -0,0 +1,40 @@
+package piscine
+
+func BasicAtoi(s string) int {
+	runes := []rune(s)
+	res := 0
+	for _, r := range runes {
+		res *= 10
+		if r == '0' {
+			res += 0
+		}
+		if r == '1' {
+			res += 1
+		}
+		if r == '2' {
+			res += 2
+		}
+		if r == '3' {
+			res += 3
+		}
+		if r == '4' {
+			res += 4
+		}
+		if r == '5' {
+			res += 5
+		}
+		if r == '6' {
+			res += 6
+		}
+		if r == '7' {
+			res += 7
+		}
+		if r == '8' {
+			res += 8
+		}
+		if r == '9' {
+			res += 9
+		}
+	}
+	return res
+}
